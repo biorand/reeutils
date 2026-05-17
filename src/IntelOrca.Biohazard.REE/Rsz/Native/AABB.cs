6d64646a05c0aa261b162e04f050ceccede43561
Ted John <ted@brambles.org>
2026-05-18 00:34:07 +0100

    Improve AABB struct

--- src/IntelOrca.Biohazard.REE/Rsz/Native/AABB.cs
+++ src/IntelOrca.Biohazard.REE/Rsz/Native/AABB.cs
@@ -2,36 +2,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
-using System.Text.Json.Serialization;
 
 namespace via
 {
     [StructLayout(LayoutKind.Explicit, Size = 32)]
     public struct AABB
     {
-        [FieldOffset(0), JsonIgnore]
-        public Vector3 minpos;
-        [FieldOffset(16), JsonIgnore]
-        public Vector3 maxpos;
+        [FieldOffset(0)]
+        public Vector3 Min;
+        [FieldOffset(16)]
+        public Vector3 Max;
 
         public AABB()
         {
         }
 
-        public AABB(Vector3 minpos, Vector3 maxpos)
+        public AABB(Vector3 min, Vector3 max)
         {
-            this.minpos = minpos;
-            this.maxpos = maxpos;
+            Min = min;
+            Max = max;
         }
 
-        public Vector3 Minpos { readonly get => minpos; set => minpos = value; }
-        public Vector3 Maxpos { readonly get => maxpos; set => maxpos = value; }
+        public readonly Vector3 Size => Max - Min;
+        public readonly Vector3 Center => (Min + Max) / 2;
 
-        public readonly Vector3 Size => maxpos - minpos;
-        public readonly Vector3 Center => (minpos + maxpos) / 2;
-
-        public readonly bool IsEmpty => minpos == maxpos;
-        public readonly bool IsInvalid => minpos.X == float.MaxValue;
+        public readonly bool IsEmpty => Min == Max;
+        public readonly bool IsInvalid => Min.X == float.MaxValue;
 
         public static readonly AABB MaxMin = new AABB(new Vector3(float.MaxValue), new Vector3(float.MinValue));
         public static readonly AABB MinMax = new AABB(new Vector3(float.MinValue), new Vector3(float.MaxValue));
@@ -39,25 +35,25 @@ namespace via
 
         public readonly AABB Extend(Vector3 point)
         {
-            return new AABB(Vector3.Min(minpos, point), Vector3.Max(maxpos, point));
+            return new AABB(Vector3.Min(Min, point), Vector3.Max(Max, point));
         }
 
         public readonly AABB Extend(AABB other)
         {
-            return new AABB(Vector3.Min(minpos, other.minpos), Vector3.Max(maxpos, other.maxpos));
+            return new AABB(Vector3.Min(Min, other.Min), Vector3.Max(Max, other.Max));
         }
 
         public readonly AABB Margin(float margin)
         {
             var mv = new Vector3(margin);
-            return new AABB(minpos - mv, maxpos + mv);
+            return new AABB(Min - mv, Max + mv);
         }
 
         public static AABB Combine(IEnumerable<AABB> bounds) => bounds.Aggregate(MaxMin, (bound, item) => bound.Extend(item));
 
-        public static AABB operator +(AABB aabb, Vector3 vec) => new AABB(aabb.minpos + vec, aabb.maxpos + vec);
-        public static AABB operator -(AABB aabb, Vector3 vec) => new AABB(aabb.minpos - vec, aabb.maxpos - vec);
+        public static AABB operator +(AABB aabb, Vector3 vec) => new AABB(aabb.Min + vec, aabb.Max + vec);
+        public static AABB operator -(AABB aabb, Vector3 vec) => new AABB(aabb.Min - vec, aabb.Max - vec);
 
-        public readonly override string ToString() => $"AABB({minpos} {maxpos})";
+        public readonly override string ToString() => $"AABB({Min} {Max})";
     }
 }
